a06f51a149621e86c9b883d90a490963d3fa502a
V1kaNikolaeva <114816050+V1kaNikolaeva@users.noreply.github.com>
2024-04-08 12:49:35 +0300

    first commit

---
+++ scripts/scrollTo.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const servises = document.querySelector('.servises__contanier');
+const ourObjects = document.querySelector('.objects__contanier');
+const contactUs = document.querySelector('.contact__contanier');
+const aboutContanier = document.querySelector('.about__contanier');
+const headerHeight = 110
+
+document.querySelector('.servises-button').addEventListener('click', () => {
+    servises.scrollIntoView();
+});
+
+document.querySelector('.objects-button').addEventListener('click', () => {
+    ourObjects.scrollIntoView();
+});
+
+document.querySelector('.contact-button').addEventListener('click', () => {
+    contactUs.scrollIntoView();
+});
+
+document.querySelector('.about-button').addEventListener('click', () => {
+    aboutContanier.scrollIntoView();
+});
