df001d4e82ab5f2e4ffe0e46f3690b3753c9f534
V1kaNikolaeva <114816050+V1kaNikolaeva@users.noreply.github.com>
2024-05-23 00:44:47 +0300

    fix bug

--- scripts/scrollTo.js
+++ scripts/scrollTo.js
@@ -4,15 +4,15 @@ const servises = document.querySelector('.servises__contanier');
 const ourObjects = document.querySelector('.objects__contanier');
 const contactUs = document.querySelector('.contact__contanier');
 const aboutContanier = document.querySelector('.about__contanier');
-const headerHeight = 110
+const headerHeight = 120
 
 document.querySelector('.servises-button').addEventListener('click', () => {
     servises.scrollIntoView();
 });
 
-document.querySelector('.objects-button').addEventListener('click', () => {
-    ourObjects.scrollIntoView();
-});
+// document.querySelector('.objects-button').addEventListener('click', () => {
+//     ourObjects.scrollIntoView();
+// });
 
 document.querySelector('.contact-button').addEventListener('click', () => {
     contactUs.scrollIntoView();
